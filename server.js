c163262a43c61727e65943994f6a2bc06e3cff20
Birznieks <bruno.birznieks@accenture.com>
2018-05-18 14:09:24 +0300

    Added a projects page

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use((req, res, next) => {
   });
   next();
 });
-app.use((req, res, next) => res.render('maintenance.hbs'));
+// app.use((req, res, next) => res.render('maintenance.hbs'));
 app.use(express.static(`${__dirname}/public`));
 hbs.registerHelper('getCurrentYear', () => new Date().getFullYear());
 hbs.registerHelper('screamIt', text => text.toUpperCase());
@@ -32,6 +32,12 @@ app.get('/', (req, res) => {
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page'
+  });
+});
+
+app.get('/projects', (req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
   });
 });
 
